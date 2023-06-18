7a6d593e9a36e39b3f338fde99874b0975827916
z3ro2201 <sinuk1991@gmail.com>
2023-06-19 01:52:16 +0900

    OG 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,25 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'start all over again',
   description: '처음부터 다시 시작하다.',
+  colorScheme: 'dark',
+  authors: {
+    name: '2ER0'
+  },
+  openGraph: {
+    title: 'start all over agin',
+    description: '처음부터 다시 시작하다.',
+    url: 'https://2er0.io',
+    siteName: '2er0.io',
+    images: [
+      {
+        url: '/elgasia.jpg',
+        width: 3408,
+        height: 1432
+      }
+    ],
+    locale: 'ko-KR',
+    type: 'website'
+  }
 }
 
 export default function RootLayout({
