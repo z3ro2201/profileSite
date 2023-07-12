eafd219399405ba6e1bb14abf718ae18088e59d6
z3ro2201 <sinuk1991@gmail.com>
2023-07-13 03:08:59 +0900

    블로그 전체 최근 post 목록 가져오는 페이지

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,15 +1,57 @@
-import React from "react";
-import Link from 'next/link';
+'use client'
+import React, {useEffect , useState} from "react";
+import axios from 'axios';
 import Gnb from './gnb'
-import Fnb from './fnb'
 
-export default function Blog() {
+interface BlogPostList {
+    id: number;
+    post_title: string;
+    post_content: string;
+    post_created_time: string;
+    post_writer_id: number;
+    post_writer_name: string;
+    post_attachment: number;
+}
+
+const Blog = () => {
+    const [postList, setPostList] = useState<BlogPostList[]>([]);
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const response = await axios.get('/api/blog/post/list');
+                const res = response.data;
+                setPostList(res.data)
+            } catch (e) {
+                console.error(e);
+            }
+            console.log(postList);
+        })();
+    }, []);
+
     return(
-        <div className="w-full h-full bg-zinc-900">
+        <div className="w-full h-full bg-zinc-900 box-border">
             <Gnb/>
             <div className="flex p-4 w-full h-12">
                 <a href="">test</a>
             </div>
+            <div className="w-full box-sizing p-2">
+                {
+                    postList.map((item, key) => (
+                        <div className="w-full my-2" key={key}>
+                            <div className="w-full p-2 my-2 bg-slate-100 text-black">{item.post_title}</div>
+                            <div className="w-full p-2 my-2" dangerouslySetInnerHTML={{__html: item.post_content}}></div>
+                            <div className="w-full p-2 my-2 bg-slate-100 text-black">{item.post_writer_name}</div>
+                            <div className="w-full p-2 my-2 bg-slate-100 text-black">{item.post_created_time}</div>
+                        </div>
+                    )
+                    
+                    )
+                    
+                }
+            </div>
         </div>
     )
 }
+
+export default Blog;
--- app/blog/write/page.tsx
+++ app/blog/write/page.tsx
@@ -1,17 +1,64 @@
 'use client'
 import React, {useState, useRef, useEffect} from "react";
 import Gnb from '../gnb'
+import axios from 'axios';
 import TinyMceEditor from '@/components/tinymceEditor';
+import {decodedPayload} from '@/controllers/sign';
 
-export default function Write() {
+interface EditorProps {
+    initialSubject: string;
+    initialContent: string;
+  }
+
+const Write: React.FC<EditorProps> = ({initialSubject, initialContent}) => {
+    const [subjectText, setSubjectText] = useState<string>('');
+    const [editorText, setEditorText] = useState<string>('');
+    const userData = decodedPayload(localStorage.getItem('key'));
+
+    const handleEditorTextChange = (text: string) => {
+        setEditorText(text);
+    }
+
+    const handleEditorTextSeverSave = async () => {
+        console.log(subjectText, editorText)
+        
+        try {
+            const response = await axios.post('/api/blog/post/write', {
+                email: userData.email,
+                subject: subjectText,
+                content: editorText
+            })
+            const res = response.data;
+            
+            if(res.status !== 200) {
+                return alert(res.data.messages);
+            }
+
+            alert("글이 정상적으로 등록되었습니다!");
+        } catch (e) {
+            alert("오류가 발생했습니다.\n오류내용:" + e);
+        }
+    }
     
+    const handleSubjectTextChange = (e: any) => {
+        setSubjectText(e.target.value)
+    }
+
+    useEffect(() => setEditorText(editorText ?? ''), [])
+
     return (
         <div className="w-full h-full bg-zinc-900">
             <Gnb/>
             <h1>글 작성하기</h1>
-            <div className="flex p-4 w-full h-auto">
-                <TinyMceEditor />
+            <div className="w-full p-2 mb-2">
+                <input type="text" id="text" name="text" className="w-full p-2" onChange={handleSubjectTextChange}/>
             </div>
+            <div className="flex p-4 w-full h-auto">
+                <TinyMceEditor editorText={initialContent} onEditorTextChange={handleEditorTextChange} />
+            </div>
+            <button onClick={handleEditorTextSeverSave}>저장</button>
         </div>
     );
 }
+
+export default Write;
