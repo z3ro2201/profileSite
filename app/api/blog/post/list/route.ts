9863e912002769b19be14cb20e46e5bf3919311d
z3ro2201 <sinuk1991@gmail.com>
2023-07-13 03:12:30 +0900

    블로그 최근 작성 글 목록

---
+++ app/api/blog/post/list/route.ts
@@ -0,0 +1,33 @@
+import { NextRequest, NextResponse } from "next/server";
+import { queryPromise } from "@/utils/mysql";
+/*
+// 사용예시
+queryPromise('SELECT * FROM users')
+    .then(rows => {
+        console.log(rows);
+    })
+    .catch(error => {
+        console.error(error);
+    });
+
+*/
+
+export async function GET(req: NextRequest, res: NextResponse){
+    try {
+        const sql = `SELECT * FROM blog_post `;
+        const rows:any = await queryPromise(sql);
+        if(rows.length === 0) return NextResponse.json({
+            status: 404,
+            messages: '등록된 글이 없습니다.'
+        })
+        const data = rows;
+        return NextResponse.json({
+            status: 200,
+            messages: 'success',
+            data: data
+        });
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({status: 500, messages: 'Internal Server Error'})
+    }
+}
