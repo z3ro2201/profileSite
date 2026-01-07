d79ab522cce0f5ae6877def3b322c4463a31a411
z3ro2201 <hello@2er0.io>
2026-01-07 10:09:28 +0900

    seo최적화 작업

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/s3",
+        permanent: false, // true = 308
+      },
+    ];
+  },
 };
 
 export default nextConfig;
