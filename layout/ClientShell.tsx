56a82c02af95686986a7e9f64198bc88b218379b
z3ro2201 <hello@2er0.io>
2026-01-08 18:47:00 +0900

    특정페이지는 배경화면 넣지 않음

--- layout/ClientShell.tsx
+++ layout/ClientShell.tsx
@@ -2,23 +2,36 @@
 
 import { useState } from "react";
 import Player from "@/components/player";
+import { usePathname } from "next/navigation";
 
 export default function ClientShell({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
   const [bgImage, setBgImage] = useState<string>("");
 
+  // blog 혹은 admin 페이지가 아닌경우에는 배경이미지를 보여줌
+  if (!pathname.startsWith("/blog") && !pathname.startsWith("/admin")) {
+    return (
+      <>
+        <div
+          style={{
+            backgroundImage: bgImage ? `url(${bgImage})` : undefined,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            height: "100vh",
+          }}
+        >
+          {children}
+        </div>
+
+        <Player onBgImageChange={setBgImage} />
+      </>
+    );
+  }
+
+  // 블로그가 아닌경우 배경이미지를 보여주지 않음
   return (
     <>
-      <div
-        style={{
-          backgroundImage: bgImage ? `url(${bgImage})` : undefined,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          height: "100vh",
-        }}
-      >
-        {children}
-      </div>
-
+      {children}
       <Player onBgImageChange={setBgImage} />
     </>
   );
