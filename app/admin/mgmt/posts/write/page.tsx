883d1b6b88162217fe92d4c7574e6031504ad5d6
z3ro2201 <hello@2er0.io>
2026-01-09 01:45:14 +0900

    ssr, csr 분리

--- app/admin/mgmt/posts/write/page.tsx
+++ app/admin/mgmt/posts/write/page.tsx
@@ -1,15 +1,14 @@
-"use client";
+import dynamic from "next/dynamic";
 
-import { useRef, useState } from "react";
+const PostEditor = dynamic(() => import("@/components/blog/Editor/PostEditor"), {
+  ssr: false,
+  loading: () => <div className="p-4">에디터 로딩중...</div>,
+});
 
-type PostStateProp = "" | "DRAFT" | "PUBLISHED" | "ARCHIVED";
-
-import PostEditor from "@/components/blog/Editor/PostEditor";
-const PostWritePage = () => {
+export default function WritePage() {
   return (
-    <>
+    <div className="p-6">
       <PostEditor PostType="new" PostId={null} />
-    </>
+    </div>
   );
-};
-export default PostWritePage;
+}
