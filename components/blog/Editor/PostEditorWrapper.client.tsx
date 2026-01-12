db4e4604328cbd5edc044fd86e6c5960fc97ee95
z3r02201 <hello@2er0.io>
2026-01-12 13:55:45 +0900

    admin post view, edit 틀 작업중

--- components/blog/Editor/PostEditorWrapper.client.tsx
+++ components/blog/Editor/PostEditorWrapper.client.tsx
@@ -1,12 +1,18 @@
 "use client";
 
+import { PublicPostDetail } from "@/types/Posts";
 import dynamic from "next/dynamic";
+
+type Props = {
+  postId?: number;
+  post?: PublicPostDetail;
+};
 
 const PostEditor = dynamic(() => import("@/components/blog/Editor/PostEditor"), {
   ssr: false,
   loading: () => <div className="p-4">에디터 로딩중...</div>,
 });
 
-export default function PostEditorWrapper() {
-  return <PostEditor PostType="new" PostId={null} />;
+export default function PostEditorWrapper({ postId, post }: Props) {
+  return <PostEditor PostType={postId ? "update" : "new"} PostId={postId ?? null} PostTitle={post?.title} PostContent={post?.contentMd} />;
 }
