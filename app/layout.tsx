a0d0f0b6750c9d96d9ed8f810a88f8394a707c51
z3ro2201 <hello@2er0.io>
2026-07-20 12:32:54 +0900

    layout 트래커 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -66,7 +66,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
         <ClientShell>{children}</ClientShell>
-        {/* <VisitorTracker /> */}
+        <VisitorTracker />
       </body>
     </html>
   );
